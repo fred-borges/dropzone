cf666427331eb0b08795a3043d626fd237ef5e51
Ulisses Borges <ulissessborgess@gmail.com>
2026-09-08 00:25:17 +0100

    chore: prepare project for Docker deployment

--- src/main/java/frederico/borges/dropzone/DropzoneApplication.java
+++ src/main/java/frederico/borges/dropzone/DropzoneApplication.java
@@ -3,21 +3,12 @@ package frederico.borges.dropzone;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @EnableScheduling
 public class DropzoneApplication {
 
 	public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
-        System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
-        System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
-
-        System.setProperty("SUPABASE_URL", dotenv.get("SUPABASE_URL"));
-        System.setProperty("SUPABASE_SECRET_KEY", dotenv.get("SUPABASE_SECRET_KEY"));
-        System.setProperty("SUPABASE_BUCKET", dotenv.get("SUPABASE_BUCKET"));
 
         SpringApplication.run(DropzoneApplication.class, args);
 	}
